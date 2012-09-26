a176aefa104578500c872b1610d703320f04e143
Santiago Gonzalez <slgonzalez@me.com>
2012-09-26 12:13:47 -0600

    Implemented binary counting as base state machine in mote code (sg)

--- mote/sunspot/New System/Mote/src/sunspot/Mote.java
+++ mote/sunspot/New System/Mote/src/sunspot/Mote.java
@@ -7,6 +7,7 @@ import com.sun.spot.resources.transducers.LEDColor;
 import com.sun.spot.resources.transducers.ITriColorLEDArray;
 import com.sun.spot.resources.transducers.SwitchEvent;
 import com.sun.spot.io.j2me.radiogram.*;
+import com.sun.spot.util.Utils;
 
 import java.io.*;
 import javax.microedition.io.*;
@@ -15,17 +16,38 @@ import javax.microedition.midlet.MIDletStateChangeException;
 
 
 public class Mote extends MIDlet implements ISwitchListener {
-    private static final int CHANGE_COLOR = 1;
-    private static final int CHANGE_COUNT = 2;
 
+    //MULTICOLOR LEDs
     private ITriColorLEDArray leds = (ITriColorLEDArray)Resources.lookup(ITriColorLEDArray.class);
+    private LEDColor[] colors = { LEDColor.RED, LEDColor.GREEN, LEDColor.BLUE };
+    //BUTTONS
     private ISwitch sw1 = (ISwitch)Resources.lookup(ISwitch.class, "SW1");
     private ISwitch sw2 = (ISwitch)Resources.lookup(ISwitch.class, "SW2");
-    private int count = -1;
-    private int color = 0;
-    private LEDColor[] colors = { LEDColor.RED, LEDColor.GREEN, LEDColor.BLUE };
-    private RadiogramConnection tx = null;
-    private Radiogram xdg;
+    //STATES
+    private static final int STATE_INITIALIZING = 0;
+    private static final int STATE_SENSING = 1;
+    private static final int STATE_TRANSMITTING = 2;
+    private int state = STATE_INITIALIZING;
+    private int motenum = -1;
+    
+    
+    public void switchReleased(SwitchEvent evt) {
+        if (evt.getSwitch() == sw1) {
+            //Blink binary mote num
+            leds.setOff();
+            Utils.sleep(200);
+            showCount(motenum, 0);
+            Utils.sleep(1000);
+            leds.setOff();
+            state = STATE_SENSING;
+        } else {
+            motenum++;
+            showCount(motenum, 0);
+        }
+    }
+
+    public void switchPressed(SwitchEvent evt) { }
+    
     
     private void showCount(int count, int color) {
         for (int i = 7, bit = 1; i >= 0; i--, bit <<= 1) {
@@ -44,35 +66,23 @@ public class Mote extends MIDlet implements ISwitchListener {
     }
     
     protected void startApp() throws MIDletStateChangeException {
-        System.out.println("Broadcast Counter MIDlet");
-        showColor(color);
-        sw1.addISwitchListener(this);
-        sw2.addISwitchListener(this);
-        try {
-            tx = (RadiogramConnection)Connector.open("radiogram://broadcast:123");
-            xdg = (Radiogram)tx.newDatagram(20);
-            RadiogramConnection rx = (RadiogramConnection)Connector.open("radiogram://:123");
-            Radiogram rdg = (Radiogram)rx.newDatagram(20);
-            while (true) {
-                try {
-                    rx.receive(rdg);
-                    int cmd = rdg.readInt();
-                    int newCount = rdg.readInt();
-                    int newColor = rdg.readInt();
-                    if (cmd == CHANGE_COLOR) {
-                        System.out.println("Received packet from " + rdg.getAddress());
-                        showColor(newColor);
-                    } else {
-                        showCount(newCount, newColor);
-                    }
-                } catch (IOException ex) {
-                    System.out.println("Error receiving packet: " + ex);
-                    ex.printStackTrace();
-                }
+        while (true) {
+            switch (state) {
+                case STATE_INITIALIZING:
+                    System.out.println("Initializing");
+                    
+                    
+                    break;
+                  
+                case STATE_SENSING:
+                
+                    break;
+            
+                case STATE_TRANSMITTING:
+                
+                    break;
             }
-        } catch (IOException ex) {
-            System.out.println("Error opening connections: " + ex);
-            ex.printStackTrace();
+        
         }
     }
 
@@ -82,30 +92,5 @@ public class Mote extends MIDlet implements ISwitchListener {
 
     protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
         // Only called if startApp throws any exception other than MIDletStateChangeException
-    }
-
-    public void switchReleased(SwitchEvent evt) {
-        int cmd;
-        if (evt.getSwitch() == sw1) {
-            cmd = CHANGE_COLOR;
-            if (++color >= colors.length) { color = 0; }
-            count = -1;
-        } else {
-            cmd = CHANGE_COUNT;
-            count++;
-        }
-        try {
-            xdg.reset();
-            xdg.writeInt(cmd);
-            xdg.writeInt(count);
-            xdg.writeInt(color);
-            tx.send(xdg);
-        } catch (IOException ex)  {
-            System.out.println("Error sending packet: " + ex);
-            ex.printStackTrace();
-        }
-    }
-
-    public void switchPressed(SwitchEvent evt) {
     }
 }
