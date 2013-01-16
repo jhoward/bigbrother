da194aecd4dc52b101a2d5bba076cd774ce046a6
James Howard <james.w.howard@gmail.com>
2013-01-16 15:18:21 -0700

    Cleaned up directories and refactored.

---
+++ mote/sunspot/WalkDirection/WalkDirection-OnSPOT/src/org/sunspotworld/demo/MainSPOT.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2006-2010 Sun Microsystems, Inc.
+ * Copyright (c) 2010 Oracle
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.sunspotworld.demo;
+
+import com.sun.spot.peripheral.radio.RadioFactory;
+import com.sun.spot.resources.Resources;
+import com.sun.spot.resources.transducers.ISwitch;
+import com.sun.spot.resources.transducers.ITriColorLED;
+import com.sun.spot.resources.transducers.ITriColorLEDArray;
+import com.sun.spot.service.BootloaderListenerService;
+import com.sun.spot.util.IEEEAddress;
+import com.sun.spot.util.Utils;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+/**
+ * The startApp method of this class is called by the VM to start the
+ * application.
+ *
+ * The manifest specifies this class as MIDlet-1, which means it will
+ * be selected for execution.
+ */
+public class MainSPOT extends MIDlet {
+
+    private ITriColorLEDArray leds = (ITriColorLEDArray) Resources.lookup(ITriColorLEDArray.class);
+    
+    protected void startApp() throws MIDletStateChangeException {
+        System.out.println("Hello, world");
+        BootloaderListenerService.getInstance().start();   // monitor the USB (if connected) and recognize commands from host
+
+        long ourAddr = RadioFactory.getRadioPolicyManager().getIEEEAddress();
+        System.out.println("Our radio address = " + IEEEAddress.toDottedHex(ourAddr));
+        
+        ISwitch sw1 = (ISwitch) Resources.lookup(ISwitch.class, "SW1");
+        ITriColorLED led = leds.getLED(0);
+        //led.setRGB(100,0,0);                    // set color to moderate red
+        led.setRGB(0,100,100);
+        
+        while (sw1.isOpen()) {                  // done when switch is pressed
+            led.setOn();                        // Blink LED
+            Utils.sleep(250);                   // wait 1/4 seconds
+            led.setOff();
+            Utils.sleep(1000);                  // wait 1 second
+        }
+        notifyDestroyed();                      // cause the MIDlet to exit
+    }
+    
+    protected void pauseApp() {
+        // This is not currently called by the Squawk VM
+    }
+    
+    /**
+     * Called if the MIDlet is terminated by the system.
+     * I.e. if startApp throws any exception other than MIDletStateChangeException,
+     * if the isolate running the MIDlet is killed with Isolate.exit(), or
+     * if VM.stopVM() is called.
+     * 
+     * It is not called if MIDlet.notifyDestroyed() was called.
+     *
+     * @param unconditional If true when this method is called, the MIDlet must
+     *    cleanup and release all resources. If false the MIDlet may throw
+     *    MIDletStateChangeException  to indicate it does not want to be destroyed
+     *    at this time.
+     */
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+        leds.setOff();
+    }
+}
