4d7be98055d6cadf2812152a1edefc1e7449026c
James Howard <james.w.howard@gmail.com>
2013-08-09 14:19:59 -0600

    day work.

--- matlab/Multinomial_Model_Test/getValid.m
+++ matlab/Multinomial_Model_Test/getValid.m
@@ -1,22 +1,20 @@
-function [keys] = getValid(data, numModels, modelLen, minLen)
+function [valid] = getValid(data, numModels, modelLen, minLen)
 %Returns all valid possibilities for the "next" data value
+    valid = {}
 
-d = zeros(size(data, 2), 2);
-for g = 1:size(data)
-    d(g, :) = rhash(data{1, g});
-end
-
-d
-keys = '';
-
-%Rules
-%   minLen - Must have a model for at least "minLen" 
-%               unless at the end of the model
-                
+    %This function is pretty easy
+    %Check the background model first
     
-    
-    %Check for "minLen" contingency
-    %data(1, end - minLen + 1:end)
-
+    if data(end, 1) == 1
+        for i = 1:numModels
+            valid{size(valid, 2) + 1} = [data [i;1]]; %#ok<AGROW>
+        end
+    elseif data(end, 2) == modelLen
+        for i = 1:numModels
+            valid{size(valid, 2) + 1} = [data [i;1]]; %#ok<AGROW>
+        end
+    elseif data(end, 2) >= minLen
+        valid{size(valid, 2) + 1} = [data [data(end, 1);(data(end, 2) + 1)]];
+    end
 end
 
--- matlab/Multinomial_Model_Test/testMultinomial.m
+++ matlab/Multinomial_Model_Test/testMultinomial.m
@@ -1,11 +1,17 @@
 %testMultinomial.m
 
+clear all;
+
 numModels = 2;
 modelLen = 2;
 minLen = 1;
 winSize = 2;
 
+%Index for keys is dim 1 - model        dim 2 - model offset
+
 %Setup all possibility hash table and initialize
+%Don't use num2str - it doesn't work with containers.Map
+
 keys = {mat2str([0;0])};
 values = {0};
 
@@ -18,3 +24,6 @@ for i = 1:numModels
 end
 
 %Now count instances
+foo = [2 1;2 2];
+
+valid = getValid(foo, numModels, modelLen, minLen);
