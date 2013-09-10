3c1cf4bb369dc185702db664b0ecf60555da647c
James Howard <james.w.howard@gmail.com>
2013-09-10 14:19:50 -0600

    mid day
    adding thesis directory structure.

---
+++ matlab/arima_forecast/Thesis/MyConstants.m
@@ -0,0 +1,8 @@
+classdef MyConstants
+    properties (Constant = true)
+        TRAIN_PERCENT = 0.6;
+        VALIDATION_PERCENT = 0.2;
+        TEST_PERCENT = 0.2;
+    end
+end
+
---
+++ matlab/arima_forecast/data_manipulation/cutdata.m
@@ -0,0 +1,8 @@
+function [train, valid, test, trainTimes, validTimes, testTimes] = cutdata(data)
+%cut data from the datastruct and return a series of arrays of times and
+%data
+    trainEnd = data.
+
+
+end
+
