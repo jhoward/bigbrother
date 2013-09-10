6d7250da8db5b179053595cc690d89f2ad93f7b4
James Howard <james.w.howard@gmail.com>
2013-09-10 14:21:29 -0600

    quick change

--- matlab/arima_forecast/Thesis/dataVisualization.m
+++ matlab/arima_forecast/Thesis/dataVisualization.m
@@ -4,6 +4,7 @@ clear all;
 dataLocation = 'C:\Users\JamesHoward\Documents\Dropbox\Projects\bigbrother\data\building\merl\data\merlDataClean.mat';
 load(dataLocation);
 
+
 train = data.data(:, 1:7800);
 test = data.data(:, 7801:end);
 trainTimes = data.times(:, 1:7800);
